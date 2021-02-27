22ea3ac087514a4d0031db00f060d57315558d7d
vcokltfre <vcokltfre@gmail.com>
2021-02-27 20:20:19 +0000

    chore: add init sql

---
+++ static/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Users (
+    id              BIGINT NOT NULL,
+    guildid         BIGINT NOT NULL,
+    xp              BIGINT NOT NULL DEFAULT 0,
+    monthly_xp      BIGINT NOT NULL DEFAULT 0,
+    banned          BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id, guildid)
+);
+
+CREATE TABLE IF NOT EXISTS Guilds (
+    id              BIGINT NOT NULL PRIMARY KEY,
+    prefix          VARCHAR(255) NOT NULL DEFAULT '!',
+    config          TEXT NOT NULL DEFAULT '{}',
+    banned          BOOLEAN NOT NULL DEFAULT FALSE
+);
