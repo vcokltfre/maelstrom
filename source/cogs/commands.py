3165dbc0b82c52a5ad9f2376bfb56f9ada26a126
svcodes <61701181+svcodes@users.noreply.github.com>
2021-02-27 22:15:40 -0600

    implement dm_rank

--- source/cogs/commands.py
+++ source/cogs/commands.py
@@ -73,6 +73,7 @@ class Commands(commands.Cog):
         algorithm = algos[guild.get("algorithm", "linear")]
         inc = guild.get("increment", 300)
         xp = user["xp"]
+        dm_rank = guild.get("dm_rank", False)
 
         level, required = algorithm.get_level(xp, inc)
 
@@ -86,7 +87,11 @@ class Commands(commands.Cog):
             name=f"{ctx.author.name} | {ctx.guild}", icon_url=str(ctx.author.avatar_url)
         )
 
-        await ctx.author.send(embed=embed)
+        if dm_rank:
+            await ctx.send(embed=embed)
+        else:
+            await ctx.author.send(embed=embed)
+        
 
     @commands.group(name="config", aliases=["cfg"])
     @commands.check_any(
